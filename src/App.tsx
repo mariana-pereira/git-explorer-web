1c85d26713cfa5a8107d89b5d2bb2ad874ddb543
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-14 16:12:13 -0300

    Configure redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-// import { Container } from './styles';
+import RepositoryList from './components/Repository';
+import store from './store';
 
-const App: React.FC = () => <div />;
+const App: React.FC = () => (
+  <Provider store={store}>
+    <RepositoryList />
+  </Provider>
+);
 
 export default App;
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore, Store } from 'redux';
+import { RepositoriesState } from './ducks/repositories/types';
+
+import rootReducer from './ducks/rootReducer';
+
+export interface ApplicationState {
+  repositories: RepositoriesState
+}
+
+const store: Store<ApplicationState> = createStore(rootReducer);
+
+export default store;
