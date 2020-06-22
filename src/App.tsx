4264a4c2a6da166845ab6d1aea17e6574f3e29e3
Mariana Pereira <marianna.22sp@gmail.com>
2020-06-22 16:29:01 -0300

    Add file for global styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux';
 import Routes from './routes';
 import store from './store';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <Provider store={store}>
     <Routes />
+    <GlobalStyle />
   </Provider>
 );
 
