0392ab305b83eadee53647451b4d1c8c43790324
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-11 14:40:16 -0300

    Add input for user search

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
 import RepositoryList from '../../components/RepositoryList';
 import User from '../../components/User';
@@ -7,6 +8,12 @@ import { Container, Content } from './styles';
 
 const Main: React.FC = () => (
   <Container>
+    <div id="input-content">
+      <input type="text" placeholder="Username" />
+      <button type="button">
+        <MdSearch size={20} color="#fff" />
+      </button>
+    </div>
     <Content>
       <User />
       <div id="space" />
--- src/pages/Main/styles.ts
+++ src/pages/Main/styles.ts
@@ -3,6 +3,38 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+
+  #input-content {
+    width: 100%;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    input {
+      width: 100%;
+      max-width: 230;
+      height: 32px;
+      margin-right: 5px;
+      border: 0;
+      padding: 0 10px;
+      border-radius: 6px;
+    }
+
+    button {
+      height: 32px;
+      width: 32px;
+      border: 0;
+      border-radius: 6px;
+      background: ${(props) => props.theme.colors.primary};
+    }
+  }
+
+  @media (min-width: 700px) {
+    input {
+      max-width: 500px;
+    }
+  }
 `;
 
 export const Content = styled.div`
@@ -11,7 +43,6 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-top: 15px;
 
   #space {
     height: 10px;
