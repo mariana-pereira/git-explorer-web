6b63532acc06380fa1b7b1ff2dcbed0c5c3e8516
Mariana Pereira <marianna.22sp@gmail.com>
2020-08-19 14:32:53 -0300

    List issues

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import formatDistance from 'date-fns/formatDistance';
+import { MdKeyboardArrowRight } from 'react-icons/md';
 import api from '../../services/api';
 
-import { Container, Content, RepositoryInfo } from './styles';
+import {
+  Container, Content, RepositoryInfo, Issue,
+} from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -23,13 +26,27 @@ interface Repository {
   pushed_at: Date;
 }
 
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
+}
+
 const Repository: React.FC = () => {
   const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
   const { params } = useRouteMatch<RepositoryParams>();
 
   useEffect(() => {
     api.get(`repos/${params.repository}`).then((response) => {
       setRepository(response.data);
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data);
     });
   }, [params.repository]);
 
@@ -74,6 +91,26 @@ const Repository: React.FC = () => {
               </li>
             </ul>
           </RepositoryInfo>
+
+          {issues && (
+            <ul>
+              {issues.map((issue) => (
+                <Issue key={issue.id}>
+                  <a href={issue.html_url} target="_blank">
+
+                    <div className="repo-info">
+                      <strong>{issue.title}</strong>
+                      <p>{issue.user.login}</p>
+                    </div>
+                    <div className="repo-icon">
+                      <MdKeyboardArrowRight size={20} />
+                    </div>
+
+                  </a>
+                </Issue>
+              ))}
+            </ul>
+          )}
         </Content>
       )}
     </Container>
--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -95,3 +95,47 @@ export const RepositoryInfo = styled.div`
     }
   }
 `;
+
+export const Issue = styled.li`
+  margin-top: 15px;
+  padding: 10px 0;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+
+  a {
+    display: flex;
+    justify-content: space-between;
+
+    .repo-info {
+      display: flex;
+      flex-direction: column;
+
+      strong {
+        font-size: 16px;
+        color: ${(props) => props.theme.colors.textTitle};
+      }
+
+      span {
+        margin-top: 10px;
+        font-size: 14px;
+        color: ${(props) => props.theme.colors.text};
+      }
+
+      p {
+        margin-top: 10px;
+        font-size: 14px;
+        color: ${(props) => props.theme.colors.text};
+      }
+    }
+
+    .repo-icon {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        color: ${(props) => props.theme.colors.text};
+      }
+    }
+  }
+  
+`;
