8f6b4df8ba7ffb2617d81242be00ad570f6d15d3
Mariana <mariprr22@gmail.com>
2024-07-02 11:45:33 -0300

    feat: add file for global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { RouterProvider } from 'react-router-dom';
 
 import { router } from './routes';
 import { store } from './store';
+import GlobalStyle from './styles/global';
 
 export function App() {
 
 	return (
 		<ReduxProvider store={store}>
 			<RouterProvider router={router} />
+			<GlobalStyle />
 		</ReduxProvider>
 	);
 }
